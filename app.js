8708fe88e5178d0158b342f48da9cb25a3625fd4
giselebury <54081443+giselebury@users.noreply.github.com>
2020-09-21 00:18:40 -0400

    variables notes

---
+++ app.js
@@ -0,0 +1,16 @@
+/* var life = 100 ; //number
+
+life = life - 10;
+
+var name = "Gisele"; //string
+
+var checkout = false; //boolean
+
+var box = null; //no value */
+
+const life = 100 //cannot be changed
+
+//if you use let the variabel can be changed
+
+
+console.log(box)
